3cac868eb0c123ed59d63a15b82c1e291310f5e8
Omis <talk2omis@gmail.com>
2022-06-24 00:04:00 +0100

    database schema

---
+++ database/migrations/2022_06_23_224726_create_wallets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['savings', 'loan', 'investment'])->default('savings');
+            $table->decimal("minimum", 10, 2);
+            $table->integer('Interest');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
---
+++ database/migrations/2022_06_23_224822_create_user_wallets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_wallet', function (Blueprint $table) {
+            $table->id();
+            $table->string('wallet_reference');
+            $table->foreignId('user_id')->references("id")->on("users");
+            $table->foreignId('wallet_id')->references("id")->on("wallets");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_wallets');
+    }
+}
